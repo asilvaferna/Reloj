5daaebfacaf2c80e901bda0dcf84d7f59fa698df
adri4silva <adri4silva@gmail.com>
2017-05-04 10:01:00 +0200

    Buttons interface class that lets the user navigate between all the posible options

---
+++ reloj/Botonera.java
@@ -0,0 +1,44 @@
+package reloj;
+
+public class Botonera {
+
+	private boolean alarmActive;
+	private boolean setHour;
+	private boolean setAlarm;
+
+	public void alarmOn() {
+		// TODO - implement Botonera.alarmOn
+		
+	}
+
+	public void alarmOff() {
+		// TODO - implement Botonera.alarmOff
+		
+	}
+
+	public void configHour() {
+		// TODO - implement Botonera.configHour
+		
+	}
+
+	public void configAlarm() {
+		// TODO - implement Botonera.configAlarm
+		
+	}
+
+	public void plusHr() {
+		// TODO - implement Botonera.plusHr
+		
+	}
+
+	public void plusMin() {
+		// TODO - implement Botonera.plusMin
+		
+	}
+
+	public void stopAlarm() {
+		// TODO - implement Botonera.stopAlarm
+		
+	}
+
+}
