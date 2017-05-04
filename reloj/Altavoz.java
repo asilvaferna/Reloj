bbbfbc7aca5acb1c22448e4d4b82c52f4f72fe22
adri4silva <adri4silva@gmail.com>
2017-05-04 09:57:17 +0200

    Speaker class with setMelody & playSound methods, that provides sound to the clock

---
+++ reloj/Altavoz.java
@@ -0,0 +1,21 @@
+package reloj;
+
+public class Altavoz {
+
+	private String melody;
+	private boolean ringOn;
+
+	public void setMelody(String melody) {
+		this.melody = melody;
+	}
+
+	/**
+	 * 
+	 * @param active
+	 */
+	public void playSound(boolean active) {
+		// TODO - implement Altavoz.playSound
+		throw new UnsupportedOperationException();
+	}
+
+}
