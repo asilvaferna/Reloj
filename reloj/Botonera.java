2a54b2f97c3302addbca896ae6e84ed103eed19d
adri4silva <adri4silva@gmail.com>
2017-05-18 09:50:19 +0200

    Cambios en la funcionalidad del reloj; ahora los botones son funcionales

--- reloj/Botonera.java
+++ reloj/Botonera.java
@@ -4,46 +4,45 @@ import java.time.LocalTime;
 
 public class Botonera {
 
-	private static boolean alarmActive;
-	private static boolean setHour;
-	private static boolean setAlarm;
+    private static boolean alarmActive;
+    private static boolean setHour;
+    private static boolean setAlarm;
 
-	public static void alarmOn() {
-		// TODO - implement Botonera.alarmOn
-		
-	}
+    public static boolean alarmOn() {
+        return alarmActive = true;
 
-	public static void alarmOff() {
-		// TODO - implement Botonera.alarmOff
-		
-	}
+    }
 
-	public static void configHour() {
-            
-            Display.showLed(true, false, true);
-            
-		
-	}
+    public static boolean alarmOff() {
+        return alarmActive = false;
 
-	public static void configAlarm() {
-		Display.showLed(false, true, true);
-		
-	}
+    }
 
-	public static LocalTime plusHr(LocalTime hora) {
-           
-            return hora.plusHours(1);
-            
-	}
+    public static void configHour() {
 
-	public static LocalTime plusMin(LocalTime hora) {
-		return hora.plusMinutes(1);
-		
-	}
+        Display.showLed(true, false, true);
 
-	public static void stopAlarm() {
-		// TODO - implement Botonera.stopAlarm
-		
-	}
+    }
+
+    public static void configAlarm() {
+        Display.showLed(false, true, true);
+
+    }
+
+    public static LocalTime plusHr(LocalTime hora) {
+
+        return hora.plusHours(1);
+
+    }
+
+    public static LocalTime plusMin(LocalTime hora) {
+        return hora.plusMinutes(1);
+
+    }
+
+    public static void stopAlarm() {
+        alarmOff();
+
+    }
 
 }
