94aa3af44a24be1e270af3589b50faa23e82d1f1
adri4silva <adri4silva@gmail.com>
2017-05-29 18:06:46 +0200

    Javadoc added

--- reloj/Botonera.java
+++ reloj/Botonera.java
@@ -1,5 +1,8 @@
 package reloj;
 
+/**
+ * Botonera class that handles the way that the user interacts with the app.
+ */
 import java.time.LocalTime;
 
 public class Botonera {
@@ -8,38 +11,70 @@ public class Botonera {
     private static boolean setHour;
     private static boolean setAlarm;
 
+    /**
+     * Checks if the alarm is on.
+     *
+     * @return true
+     */
     public static boolean alarmOn() {
         return alarmActive = true;
 
     }
 
+    /**
+     * Checks if the alarm is off.
+     *
+     * @return false
+     */
     public static boolean alarmOff() {
         return alarmActive = false;
 
     }
 
+    /**
+     * ConfigHour mode that turns the leds for this mode.
+     */
     public static void configHour() {
 
         Display.showLed(true, false, true);
 
     }
 
+    /**
+     * ConfigAlarm mode that turns the leds for this mode.
+     */
     public static void configAlarm() {
         Display.showLed(false, true, true);
 
     }
 
+    /**
+     * Adds 1 unit to the hour variable.
+     * @param hora
+     * @return hora + 1 unit
+     */
+    
     public static LocalTime plusHr(LocalTime hora) {
 
         return hora.plusHours(1);
 
     }
+    
+    /**
+     * Adds 1 unit the minute variable
+     * @param hora
+     * @return minute + 1 unit
+     */
 
     public static LocalTime plusMin(LocalTime hora) {
         return hora.plusMinutes(1);
 
     }
 
+    /**
+     * Sets the alarm off. This method calls to the alarmOff() method.
+     */
+    
     public static void stopAlarm() {
         alarmOff();
 
