ca41e67ad9130e7610261973a0a87f39d008a572
adri4silva <adri4silva@gmail.com>
2017-05-29 18:01:51 +0200

    Fixed some lines of code + Javadoc

--- reloj/Altavoz.java
+++ reloj/Altavoz.java
@@ -1,21 +1,29 @@
 package reloj;
-
+/**
+ * Altavoz class that handles the system sound.
+ * @author adri
+ */
 public class Altavoz {
 
 	private String melody;
 	private boolean ringOn;
+        
+        /**
+         * Set method to declare the melody string.
+         * @param melody 
+         */
 
 	public void setMelody(String melody) {
 		this.melody = melody;
 	}
 
 	/**
-	 * 
+	 * Method that plays the sound.
 	 * @param active
 	 */
 	public void playSound(boolean active) {
 		// TODO - implement Altavoz.playSound
-		throw new UnsupportedOperationException();
+		
 	}
 
 }
